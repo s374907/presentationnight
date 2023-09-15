93983a146d508c279f1ad035de6120c263c49d39
s473907 <113636030+s473907@users.noreply.github.com>
2023-09-15 03:36:13 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Page2 from "../slides/+2.svelte";
+    let page = 1
+    function onKeyDown(e){
+        if(e.keyCode == 39){
+            page += 1
+        }
+        if(e.keyCode == 37){
+            page -= 1
+        }
+    }
+</script>
+<h1>Page {page}</h1>
+{#if page == 1}
+<h1 class="text-3xl font-bold underline text-center center">
+    Hello world!
+</h1>
+{/if}
+{#if page == 2}
+<Page2/>
+{/if}
+<svelte:window on:keydown|preventDefault={onKeyDown} />
+  
+  <style lang="postcss">
+    :global(html) {
+        background: #F9C784;
+        background: linear-gradient(120deg, #F9C784 0%, #FF8C42 100%);
+    }
+  </style>
