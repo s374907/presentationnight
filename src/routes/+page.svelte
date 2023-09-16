a1c8e2d2115e3f824335ba91de568021d99b6666
QuiescentTrip <bluevs10@hotmail.no>
2023-09-16 16:30:57 +0200

    part 1 done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,12 +31,85 @@ on:keydown|preventDefault={onKeyDown}
 {/if}
 
 {#if page == 2}
-<Titlepic title="The entire history of jojo" src='images/jojo_logo.jpg'/>
+<Titlepic title="The entire history of jojo (from my memory)" src='images/part1/jojo_logo.jpg'/>
 {/if}
 
 {#if page == 3}
-<Fullimage src='images/stonemask.png'></Fullimage>
+<Fullimage src='images/part1/stonemask.png'></Fullimage>
 {/if}
+
+{#if page == 4}
+<Titlepic title="Part 1: Phantom blood"src='images/part1/phantom_blood.jpg'/>
+{/if}
+
+{#if page == 5}
+<Fullimage src='images/part1/George.webp'/>
+{/if}
+
+{#if page == 6}
+<Fullimage src='images/part1/Brando_dying.webp'/>
+{/if}
+
+{#if page == 7}
+<Fullimage src='images/part1/jonathan.jpg_large'/>
+{/if}
+
+{#if page == 8}
+<Fullimage src='images/part1/danny.jpg'/>
+{/if}
+
+{#if page == 9}
+<Fullimage src='images/part1/Dio_and_jojo.webp'/>
+{/if}
+
+{#if page == 10}
+<Fullimage src='images/part1/danny_die.jpg'/>
+{/if}
+
+{#if page == 11}
+<Fullimage src='images/part1/danny_burn.jpg'/>
+{/if}
+
+{#if page == 12}
+<Fullimage src='images/part1/dio_kiss.jpg'/>
+{/if}
+
+{#if page == 13}
+<Fullimage src='images/part1/dio_father.webp'/>
+{/if}
+
+{#if page == 14}
+<Fullimage src='images/part1/dio_mask.jpg'/>
+{/if}
+
+{#if page == 15}
+<Fullimage src='images/part1/zeppeli.jpg'/>
+{/if}
+
+{#if page == 16}
+<Fullimage src='images/part1/punch_frog.jpg'/>
+{/if}
+
+{#if page == 17}
+<Fullimage src='images/part1/dio_dies.webp'/>
+{/if}
+{#if page == 18}
+<Fullimage src='images/part1/sailing_to_america.jpg'/>
+{/if}
+
+{#if page == 19}
+<Fullimage src='images/part1/dio_fight.jpg'/>
+{/if}
+
+{#if page == 20}
+<Fullimage src='images/part1/jonathan_dies.jpg'/>
+{/if}
+
+{#if page == 21}
+<Fullimage src='images/part1/Erina_escapes.jpg'/>
+{/if}
+
+
 
 </main>
 
--- src/slides/title.svelte
+++ src/slides/title.svelte
@@ -1,9 +1,10 @@
 <script>
     export let title;
+    export let src;
 </script>
 
-<div class="flex h-screen">
-    <div class="m-auto">
+<div class="flex h-screen p-40 ">
+    <div class="m-auto bg-[url('{src}')] bg-cover">
 
     <h1 class="text-9xl font-bold text-center center text-white">
         {title}
